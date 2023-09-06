2955658c47aea3e63b7af81dc620fbb4a52d1a16
shaikmoin16 <143272459+shaikmoin16@users.noreply.github.com>
2023-09-06 13:28:21 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+var nav = document.querySelector('nav');
+var button = document.querySelector('.hamburger-menu')
+button.addEventListener('click', (event) => {
+    nav.classList.toggle('open')
+})
+
